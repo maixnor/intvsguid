10b472184edc34bf599638ca0f68d79c7f16a123
maixnor <46966993+maixnor@users.noreply.github.com>
2022-02-18 13:52:56 +0100

    cleaner code, not stopwatch

---
+++ JoinPerformanceTests/Werkbank.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace JoinPerformanceTests;
+
+[MemoryDiagnoser]
+public class Werkbank
+{
+    private readonly GuidContext _guids;
+    private readonly IntContext _ints;
+
+    public Werkbank()
+    {
+        _guids = new GuidContext();
+        _ints = new IntContext();
+        _guids.Database.EnsureDeleted();
+        _guids.Database.EnsureCreated();
+        _ints.Database.EnsureDeleted();
+        _ints.Database.EnsureCreated();
+        _guids.Seed();
+        _ints.Seed();
+    }
+
+    [Benchmark]
+    public void JoinAllGuids()
+    {
+        var people = _guids.People.Take(100).ToList();
+        foreach (var person in people)
+        {
+            _ = person.Emails.ToList();
+        }
+    }
+
+    [Benchmark]
+    public void JoinAllInts()
+    {
+        var people = _ints.People.Take(100).ToList();
+        foreach (var person in people)
+        {
+            _ = person.Emails.ToList();
+        }
+    }
+}
